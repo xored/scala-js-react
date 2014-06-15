75dd1d91ee9fb9742cf30b6c1c6680cb2fee1d17
Gleb Kanterov <gleb@kanterov.ru>
2014-06-15 15:50:54 +0700

    DRY build file

--- project/ScalaJSReact.scala
+++ project/ScalaJSReact.scala
@@ -5,13 +5,18 @@ import scala.scalajs.sbtplugin.ScalaJSPlugin._
 
 object ScalaJSReact extends Build {
 
+  val SCALA_VERSION = "2.11.1"
+
+  val scalajsDom = "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
+  val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.0.2"
+  val scalaReflect = "org.scala-lang" % "scala-reflect" % SCALA_VERSION
+  val macroParadisePlugin = compilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
+
   val commonSettings = Seq(
     version := "0.1-SNAPSHOT",
     organization := "org.scala-lang.modules.scalajs",
-    scalaVersion := "2.11.1",
-    homepage := Some(url("http://github.com/xored/scalajs-react/")),
-    // TODO investigate possibility to build for 2.10.4
-    crossScalaVersions := Seq("2.11.1")
+    scalaVersion := SCALA_VERSION,
+    homepage := Some(url("http://github.com/xored/scala-js-react/"))
   )
 
   lazy val react = Project("scalajs-react", file("scalajs-react"))
@@ -31,9 +36,9 @@ object ScalaJSReact extends Build {
     .settings(commonSettings: _*)
     .settings(
       libraryDependencies ++= Seq(
-        "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6",
-        "org.scala-lang.modules" %% "scala-xml" % "1.0.2",
-        compilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
+        scalajsDom,
+        scalaXml,
+        macroParadisePlugin
       )
     )
     .dependsOn(react)
