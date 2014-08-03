1ee04711a0dc25e8ad4aa38a5d57dd045a48d853
Gleb Kanterov <gleb@kanterov.ru>
2014-08-03 12:20:16 +0700

    Remove obsolete code

--- scalajs-react/src/main/scala/com/xored/scalajs/react/ThisLike.scala
+++ scalajs-react/src/main/scala/com/xored/scalajs/react/ThisLike.scala
@@ -16,8 +16,6 @@
 
 package com.xored.scalajs.react
 
-import scala.scalajs.js
-
 trait ThisLike {
   type Props
   type State
@@ -35,11 +33,3 @@ object ThisLike {
   val REACT_REF = "ref"
 }
 
-trait JsBridgeLike[This <: ThisLike] {
-  def mkReactProps(props: This#Props): js.Dictionary[js.Any]
-  def mkReactState(state: This#State): js.Dictionary[js.Any]
-
-  def dynamicAsThis(dynamic: js.Dynamic): This
-  def dynamicAsProps(dynamic: js.Dynamic): This#Props
-  def dynamicAsState(dynamic: js.Dynamic): This#State
-}
